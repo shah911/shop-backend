77893be6089cb5b564d9bb5fc1c5cbecb455081b
shah911 <118245581+shah911@users.noreply.github.com>
2023-07-16 11:10:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,12 @@ const connectDB = async () => {
   }
 };
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(cors());
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
